b520877e8676f9022b3d938ccef90d7191073b21
Sam Reedy <samuel.reedy@hotmail.com>
2021-09-23 21:07:37 +1000

    added Soundcloud API widget to mixes

--- mixes/script-mixes.js
+++ mixes/script-mixes.js
@@ -1,26 +1,17 @@
-(function () {
+(function createSet() {
+
+  let SOUNDCLOUD_URL = 'https://soundcloud.com/404crew/404-selects-podcast-series-14-ryan-moon', 
+      WIDGET_OPTIONS = '&color=3C9FCE&liking=false';
   
-    var SOUNDCLOUD_URL = 'http://soundcloud.com/georgconrad/shostakovich2', 
-        // You can also specify widget options in your request
-        WIDGET_OPTIONS = '&color=3C9FCE&liking=false';
-    
-    jQuery
-      .getJSON( 'http://soundcloud.com/oembed.json?url=' + SOUNDCLOUD_URL + WIDGET_OPTIONS )
-      .done( function ( data ) {
-        var widget;
-        // data.html will contain widget HTML that you can embed
-        $('body').html( data.html );
-        // For the following code to work, you need to have Widget HTML5 API 
-        // available by the time this code is invoked. The code to put API file
-        // in your page is:
-        // <script src="//w.soundcloud.com/player/api.js"></script>
-        //
-        // Create API enabled reference to the widget (note that we are passing DOM object)
-        widget = SC.Widget($('body').find('iframe')[0]);
-        // Interact with widget via API
-        widget.bind('ready', function () {
-          alert('widget ready');
-        });
-      });
-      
-    }());
+  jQuery
+  //API call
+  .getJSON( 'http://soundcloud.com/oembed.json?url=' + SOUNDCLOUD_URL + WIDGET_OPTIONS )
+  .done( function ( data ) {
+    let widget;
+    // data.html will contain widget HTML 
+    $('body').html( data.html );
+
+    // Create API enabled reference to the widget by passing DOM Obj
+    widget = SC.Widget($('body').find('iframe')[0]); 
+  });
+}());
